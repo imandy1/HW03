6869a16ff16fb2d2fc20304a064da1e12518aab1
Andy <32346823+imandy1@users.noreply.github.com>
2017-10-01 22:42:34 -0700

    setup all the classes

---
+++ src/Array2D.java
@@ -0,0 +1,21 @@
+
+public class Array2D <T>{
+	private int rows;
+	private int cols;
+	private Array2DNode head;
+	
+	public Array2D(){
+		
+	}
+	public Array2D(int rows, int cols){
+		this.rows = rows;
+		this.cols = cols;
+	}
+	//this constructor should take a generic 2d array
+	public Array2D(int[][] a){
+		
+	}
+
+
+	
+}
---
+++ src/Array2DNode.java
@@ -0,0 +1,14 @@
+
+public class Array2DNode <T>{
+	private T item;
+	private Array2DNode down;
+	private Array2DNode right;
+	
+	
+	public Array2DNode(){
+		
+	}
+	public Array2DNode(T item){
+		this.item = item;
+	}
+}
